4e5b42c313438d41f4367c6c73994da2ec2088dd
Eyimofe Blessing-Kayode <mofekayode@Eyimofes-MacBook-Pro-2.local>
2022-07-08 12:08:30 -0700

    first code

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+
+import styled from "styled-components";
+import { colors } from "../Shared/colors";
+
+const Header = ({}) => {
+  return (
+    <p>header</p>
+  );
+};
+
+
+export default Header;
---
+++ src/Shared/typography.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+import { colors } from "./colors";
+
+export const H1 = styled.h1`
+  font-family: "Antonio";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 80px;
+  line-height: 104px;
+  text-transform: uppercase;
+  color: ${colors.white};
+`;
+export const H2 = styled.h2`
+  font-family: "Antonio";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 40px;
+  line-height: 52px;
+  letter-spacing: -1.5px;
+  text-transform: uppercase;
+  color: ${colors.white};
+`;
+
+export const H3 = styled.h3`
+  font-family: "Spartan";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 12px;
+  line-height: 25px;
+  letter-spacing: 2.57143px;
+  text-transform: uppercase;
+  color: ${colors.white};
+`;
+export const H4 = styled.h4`
+  font-family: "Spartan";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 11px;
+  line-height: 25px;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: ${colors.white};
+`;
