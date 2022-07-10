c52ee208ec8a6b79a1ba345e2fc7f352763f6335
Laura Eno <enonyenooke@gmail.com>
2022-07-10 09:01:10 +0100

    Changed fonts to rems

--- src/Shared/typography.js
+++ src/Shared/typography.js
@@ -5,8 +5,8 @@ export const H1 = styled.h1`
   font-family: "Antonio";
   font-style: normal;
   font-weight: 400;
-  font-size: 80px;
-  line-height: 104px;
+  font-size: 5rem;
+  line-height: 6.4375rem;
   text-transform: uppercase;
   color: ${colors.white};
 `;
@@ -14,8 +14,8 @@ export const H2 = styled.h2`
   font-family: "Antonio";
   font-style: normal;
   font-weight: 400;
-  font-size: 40px;
-  line-height: 52px;
+  font-size: 2.5rem;
+  line-height: 3.25rem;
   letter-spacing: -1.5px;
   text-transform: uppercase;
   color: ${colors.white};
@@ -25,8 +25,8 @@ export const H3 = styled.h3`
   font-family: "Spartan";
   font-style: normal;
   font-weight: 700;
-  font-size: 12px;
-  line-height: 25px;
+  font-size: 0.75rem;
+  line-height: 1.5625rem;
   letter-spacing: 2.57143px;
   text-transform: uppercase;
   color: ${colors.white};
@@ -35,8 +35,18 @@ export const H4 = styled.h4`
   font-family: "Spartan";
   font-style: normal;
   font-weight: 700;
-  font-size: 11px;
-  line-height: 25px;
+  font-size: 0.6875rem;
+  line-height: 1.5625rem;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  color: ${colors.white};
+`;
+
+export const p = styled.p`
+  font-family: "Spartan";
+  font-style: normal;
+  font-size: 0.875rem;
+  line-height: 1.5625rem;
   letter-spacing: 1px;
   text-transform: uppercase;
   color: ${colors.white};
