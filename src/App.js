5a35d1b6192aa18ec4f1ef2d90fe7eb33425b6ce
Laura Eno <enonyenooke@gmail.com>
2022-07-11 20:12:02 +0100

    Worked on backgroud

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import { colors } from "./Shared/colors";
 import { H1, H2, H3, H4 } from "./Shared/typography";
 import Header from "./Components/Header";
 import background from "./assets/background-stars.svg"
+
 function App() {
   const [active, setActive] = useState(false);
 
   return (
-    <div className="App" style={{backgroundColor:"#070724",backgroundImage:`url(${background})`, backgroundSize: "cover"}}>
+    <div className="App">
      <Header/>
      <Button> Buton</Button>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import './index.css'
 import reportWebVitals from "./reportWebVitals";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+document.body.style = 'background: red'
 root.render(
   <React.StrictMode>
     <App />
