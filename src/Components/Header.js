d112678e1b6e335d402f3173cfa014325810e7a5
Laura Eno <enonyenooke@gmail.com>
2022-07-15 18:49:55 +0100

    Header for all screen sizes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,14 +2,15 @@
 import styled from "styled-components";
 import { colors } from "../Shared/colors";
 import { H1, H2, H3, H4, P } from "../Shared/typography";
+import nav from "../assets/icon-hamburger.svg"
 
 const Header = ({}) => {
   return (
     <StyledHeader>
-      <section>
+      <Logo>
         <H2>The planets</H2>
-      </section>
-      <Nav style={{cursor:'pointer'}}>
+      </Logo>
+      <Nav>
         <H4>Mercury</H4>
         <H4>Venus</H4>
         <H4>Earth</H4>
@@ -19,39 +20,56 @@ const Header = ({}) => {
         <H4>Uranus</H4>
         <H4>Neptune</H4>
       </Nav>
+      <HamBurger>
+        <img src= {nav} />
+      </HamBurger>
     </StyledHeader>
   );
 };
 
 
- const StyledHeader= styled.header`
+export const StyledHeader= styled.header`
   color: ${colors.white};
   background-color: ${(props) =>
     props.active ? props.activeColor : props.background};
   display: flex;
   justify-content: space-between;
   border-bottom: 1px solid ${colors.lightGrey};
-  padding: 0.725em 1.5em;
-  @media only screen and (max-width: 768px) {
-   flex-direction:column;
-   align-items:center;
-   padding: 1.5em 1.5em;
+  padding: 1.5em 1.5em;
+  
+  @media only screen and (min-width: 768px){
+    flex-direction: column;
+    align-items:center;
+    padding: 1.5em 1.5em;
   }
-  @media only screen and (max-width: 375px) {
+  @media only screen and (min-width: 1440px){
+    flex-direction: row;
+  }
+`;
+
+const Logo = styled.section`
+    flex: 1;
+`
+
+const Nav = styled.nav`
+  display: none;
+  @media only screen and (min-width: 768px){
+    display: flex;
+    width: 100%;
+    margin-top: 1.5em;
+    justify-content: space-between;
+    align-items: center;
+  }
+  @media only screen and (min-width: 768px){
+    flex: 1;
   }
 `
 
-const Nav= styled.nav`
-  display: flex;
-  justify-content: space-between;
-  width: clamp(500px,50%,1000px);
-  align-items: center;
-  cursor:pointer;
-  @media only screen and (max-width: 768px) {
-    width:100%;
-    margin-top:20px
+const HamBurger = styled.section`
+  margin: auto 0;
+  @media only screen and (min-width: 768px){
+    display: none;
   }
 `
-
 
 export default Header;
